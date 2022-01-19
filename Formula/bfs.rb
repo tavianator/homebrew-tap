250f436a2d5d2236981a41c60bef5e5393708682
Tavian Barnes <tavianator@tavianator.com>
2022-01-19 14:16:22 -0500

    bfs: Version 2.3

--- Formula/bfs.rb
+++ Formula/bfs.rb
@@ -1,8 +1,8 @@
 class Bfs < Formula
   desc "Breadth-first version of find."
   homepage "https://github.com/tavianator/bfs"
-  url "https://github.com/tavianator/bfs/archive/2.0.tar.gz"
-  sha256 "afbde70742f4bac3f3b030b36531fe0bd67dbdda61ce0457f3a1e5681405df33"
+  url "https://github.com/tavianator/bfs/archive/2.3.tar.gz"
+  sha256 "15d5455af3a399561db1d701f2032d743a7a1a201d60df2b55cafd3517277bd3"
   head "https://github.com/tavianator/bfs.git"
 
   def install
@@ -11,6 +11,8 @@ class Bfs < Formula
     bin.install "bfs"
 
     man1.install "bfs.1"
+
+    bash_completion.install "completions/bfs.bash" => "bfs"
 
     pkgshare.install "tests.sh"
     pkgshare.install Dir["tests"]
