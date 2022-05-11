a2c874e7a107c1fc9f11d9a3d1f28b94e01d7f03
Tavian Barnes <tavianator@tavianator.com>
2022-05-11 13:55:09 -0400

    bfs: Version 2.5

--- Formula/bfs.rb
+++ Formula/bfs.rb
@@ -1,8 +1,8 @@
 class Bfs < Formula
   desc "Breadth-first version of find."
   homepage "https://github.com/tavianator/bfs"
-  url "https://github.com/tavianator/bfs/archive/2.3.1.tar.gz"
-  sha256 "c3ed60001804ca79a3794935ab6b9927bd14df42dc34e475b34d62929fcc5a98"
+  url "https://github.com/tavianator/bfs/archive/2.5.tar.gz"
+  sha256 "410be5de74bfc5dece213731d3a8462310789118d299fb14f02bd9b0fdd66875"
   head "https://github.com/tavianator/bfs.git"
 
   def install
