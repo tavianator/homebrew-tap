940180bf831404cd71e1b7d26563c29cc29989f1
Tavian Barnes <tavianator@tavianator.com>
2022-06-01 13:15:48 -0400

    bfs: Version 2.6

--- Formula/bfs.rb
+++ Formula/bfs.rb
@@ -1,21 +1,13 @@
 class Bfs < Formula
   desc "Breadth-first version of find."
   homepage "https://github.com/tavianator/bfs"
-  url "https://github.com/tavianator/bfs/archive/2.5.tar.gz"
-  sha256 "410be5de74bfc5dece213731d3a8462310789118d299fb14f02bd9b0fdd66875"
+  url "https://github.com/tavianator/bfs/archive/2.6.tar.gz"
+  sha256 "5812a4c62004c851eb02e23c0b6698dbb40061ce1c4069bb33c5b14f5361d81b"
   head "https://github.com/tavianator/bfs.git"
   depends_on "oniguruma"
 
   def install
-    system "make", "release", "all"
-
-    bin.install "bfs"
-
-    man1.install "bfs.1"
-
-    bash_completion.install "completions/bfs.bash" => "bfs"
-
-    pkgshare.install "tests.sh"
-    pkgshare.install Dir["tests"]
+    system "make", "release"
+    system "make", "install", "PREFIX=#{prefix}"
   end
 end
