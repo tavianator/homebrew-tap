44f2f093ff61e495ae2d697dfd3bb40e7f68e7f1
Tavian Barnes <tavianator@tavianator.com>
2022-07-06 11:05:07 -0400

    bfs: Version 2.6.1

--- Formula/bfs.rb
+++ Formula/bfs.rb
@@ -1,8 +1,8 @@
 class Bfs < Formula
   desc "Breadth-first version of find."
   homepage "https://github.com/tavianator/bfs"
-  url "https://github.com/tavianator/bfs/archive/2.6.tar.gz"
-  sha256 "5812a4c62004c851eb02e23c0b6698dbb40061ce1c4069bb33c5b14f5361d81b"
+  url "https://github.com/tavianator/bfs/archive/2.6.1.tar.gz"
+  sha256 "15e35b46774af305cbdfbfaa9889591440579a8233bc7308e050ec27b791647d"
   head "https://github.com/tavianator/bfs.git"
   depends_on "oniguruma"
 
