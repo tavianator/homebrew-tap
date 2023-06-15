580530adab2104514ed1e05e8057ae641dde96e5
Tavian Barnes <tavianator@tavianator.com>
2023-06-15 11:11:17 -0400

    bfs: Update to 2.6.3
    Closes #5.

--- Formula/bfs.rb
+++ Formula/bfs.rb
@@ -1,8 +1,8 @@
 class Bfs < Formula
   desc "Breadth-first version of find."
   homepage "https://github.com/tavianator/bfs"
-  url "https://github.com/tavianator/bfs/archive/2.6.2.tar.gz"
-  sha256 "1d50f7fdeda2bafd3ce6e4eacbe88de43f3ec14fb904aa8f81641aefba465afe"
+  url "https://github.com/tavianator/bfs/archive/2.6.3.tar.gz"
+  sha256 "b88b7d8f8c5f701209fd2967546ad154c405281749ad458bc116b40da14b98f6"
   head "https://github.com/tavianator/bfs.git"
   depends_on "oniguruma"
 
